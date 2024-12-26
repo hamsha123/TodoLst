9e471c1e56b72bed1c6b7238da70f86738b2d471
hamsha123 <hamsha.mahesan@gmail.com>
2024-12-26 19:33:14 +0800

    updated
    updated script

--- toDoTest/addItems.test.js
+++ toDoTest/addItems.test.js
@@ -7,6 +7,8 @@ test.describe('Add Todo Items', () => {
     await page.goto('https://todomvc.com/examples/react/dist/');
   });
 
+//Add task
+
   test('should add multiple todo items', async ({ page }) => {
     const todoItems = ['rest', 'test', 'shop', 'gym', 'read', 'walk'];
 
@@ -28,8 +30,8 @@ test.describe('Add Todo Items', () => {
     }
 
    // Verify the items left count
-  const itemsLeft = await page.textContent('.todo-count');
-  console.log(`Items left: ${itemsLeft}`); // Expected: "2 items left"
+   let itemsLeft = await page.textContent('.todo-count');
+  console.log(`Items left: ${itemsLeft}`); 
 
 //Complete the task
 
@@ -55,7 +57,7 @@ test.describe('Add Todo Items', () => {
 
 // Edit task 
   const initialItemText = 'rest';
-  const editedTodoText = 'rest2';
+  const editedTodoText = 'swim';
 
   // Double-click the todo item to edit it
   const todoLocator1= page.locator(`.todo-list li:has-text("${initialItemText}")`);
@@ -68,8 +70,42 @@ test.describe('Add Todo Items', () => {
 
 // Verify the changes are reflected in the todo list
   await expect(page.locator(`.todo-list li:has-text("${editedTodoText}")`).first()).toBeVisible({ timeout: 10000 });
+  await expect(page.locator(`.todo-list li:has-text("${initialItemText}")`)).toHaveCount(0, { timeout: 10000 });
 
+//Delete task
+// Hover over the todo item to reveal the delete button
+const todoText = 'test';
+const todoLocator2 = page.locator(`.todo-list li:has-text("${todoText}")`);
+await todoLocator2.hover();
 
+// Click the delete button
+//const deleteButton = todoLocator2.locator('.destroy');
+const deleteButton = todoLocator2.getByRole('button', { name: '×' });
+await deleteButton.click();
 
+// Verify the todo item is removed from the list
+await expect(page.locator(`.todo-list li:has-text("${todoText}")`)).toHaveCount(0, { timeout: 10000 });
+
+// Verify the items left count
+itemsLeft = await page.textContent('.todo-count');
+console.log(`Items left: ${itemsLeft}`);
+
+// Click the "Completed" filter button
+await page.click('text=Completed');
+await expect(page.locator('.todo-list li')).toHaveCount(1);
+itemsLeft = await page.textContent('.todo-count');
+console.log(`Items left: ${itemsLeft}`);
+
+// Click the "Active" filter button
+await page.click('text=Active');
+await expect(page.locator('.todo-list li')).toHaveCount(4);
+itemsLeft = await page.textContent('.todo-count');
+console.log(`Items left: ${itemsLeft}`);
+
+// Click the "All" filter button
+await page.click('footer >> text=All');
+await expect(page.locator('.todo-list li')).toHaveCount(5);
+itemsLeft = await page.textContent('.todo-count');
+console.log(`Items left: ${itemsLeft}`);
   });
 });
